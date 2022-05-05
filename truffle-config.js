35eecb0b7175e237ed8a05807a082ec2be80aed1
XanMarta <gmxancrazy24114@gmail.com>
2022-05-06 00:02:56 +0700

    complete testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,23 @@
 const path = require("path");
+const { projectID, mnemonic } = require("./secrets.json")
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+
 module.exports = {
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "/build"),
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" //Match any network id
+      network_id: "*"
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectID}`, 0),
+      network_id: 3,
+      gas: 4000000,
+      gasPrice: 100000000000,
+      confirmations: 2,
+      timeoutBlocks: 5000,
+      skipDryRun: true
     }
   }
 };
